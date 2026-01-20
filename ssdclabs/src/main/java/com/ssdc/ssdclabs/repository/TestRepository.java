25c92a3151b949fe1ed306642e4efb4cf36ec7f3
root <root@vmi3010073.contaboserver.net>
2026-01-20 16:20:04 +0530

    Update frontend UI and backend logic

--- ssdclabs/src/main/java/com/ssdc/ssdclabs/repository/TestRepository.java
+++ ssdclabs/src/main/java/com/ssdc/ssdclabs/repository/TestRepository.java
@@ -3,10 +3,8 @@ package com.ssdc.ssdclabs.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.ssdc.ssdclabs.model.Test;
 
@@ -33,14 +31,5 @@ public interface TestRepository extends JpaRepository<Test, Long> {
     """)
     boolean isTestUsed(@Param("testId") Long testId);
 
-    /* ================= DRAG & DROP UPDATE ================= */
-    @Modifying
-    @Transactional
-    @Query("""
-        UPDATE Test t
-        SET t.displayOrder = :order
-        WHERE t.id = :id
-    """)
-    void updateOrder(@Param("id") Long id,
-                     @Param("order") int order);
+ 
 }
